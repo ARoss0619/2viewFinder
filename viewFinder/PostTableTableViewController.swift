e84ae7f7d0f1f1268af71fbf9b63715a742e1686
Amber Ross <ross.amber1540@gmail.com>
2019-06-25 12:49:19 -0400

    complete interation 3

--- viewFinder/PostTableTableViewController.swift
+++ viewFinder/PostTableTableViewController.swift
@@ -60,7 +60,20 @@ class PostTableTableViewController: UITableViewController {
         return cell
     }
  
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "detailSegue", sender: photos[indexPath.row])
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue" {
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+                if let photoToSend = sender as? Photos {
+                    photoDetailView.photo = photoToSend
+                }
+            }
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
